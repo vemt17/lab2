dec3db6ccc5e42c00e03d1527b2bed7e8651cfbb
vemt17 <vmtan@student.apc.edu.ph>
2023-02-05 12:56:50 +0800

    form validation

--- week9/index.php
+++ week9/index.php
@@ -133,9 +133,56 @@ alert("Welcome to my Profile Page!");
         </div>
 
     </div>
+    <?php
+    // define variables and set to empty values
+    $name = $email = $gender = $comment = $website = "";
 
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      $name = test_input($_POST["name"]);
+      $email = test_input($_POST["email"]);
+      $website = test_input($_POST["website"]);
+      $comment = test_input($_POST["comment"]);
+      $gender = test_input($_POST["gender"]);
+    }
+
+    function test_input($data) {
+      $data = trim($data);
+      $data = stripslashes($data);
+      $data = htmlspecialchars($data);
+      return $data;
+    }
+    ?>
+
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+      Name: <input type="text" name="name">
+      <br><br>
+      E-mail: <input type="text" name="email">
+      <br><br>
+      Website: <input type="text" name="website">
+      <br><br>
+      Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+      <br><br>
+      Gender:
+      <input type="radio" name="gender" value="female">Female
+      <input type="radio" name="gender" value="male">Male
+      <input type="radio" name="gender" value="other">Other
+      <br><br>
+      <input type="submit" name="submit" value="Submit">  
+    </form>
+
+    <?php
+    echo "<h2>Your Input:</h2>";
+    echo $name;
+    echo "<br>";
+    echo $email;
+    echo "<br>";
+    echo $website;
+    echo "<br>";
+    echo $comment;
+    echo "<br>";
+    echo $gender;
+    ?>   
 </div>
-
 </section>
 
 <!-- contact section ends -->
