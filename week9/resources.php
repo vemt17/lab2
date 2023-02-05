093cf4d17511871b25cdea68829dc001bc4d698e
vemt17 <vmtan@student.apc.edu.ph>
2023-02-05 12:48:13 +0800

    form

---
+++ week9/resources.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resources</title>
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    
+
+<!-- about section starts  -->
+
+<section class="about" id="about">
+
+<h1 class="heading"> <span>Resources</span></h1>
+
+<div class="row">
+
+    <div class="info">
+        <h3> <span> HTML : </span> <a href="https://my-learning.w3schools.com/tutorial/html">W3Schools</a> </h3>
+        <h3> <span> CSS : </span> <a href="https://my-learning.w3schools.com/tutorial/css">https://my-learning.w3schools.com/tutorial/css</a> </h3>
+        <h3> <span> Javascript : </span> <a href="https://my-learning.w3schools.com/tutorial/js">https://my-learning.w3schools.com/tutorial/js</a> </h3>
+        <h3> <span> Website : </span> <a href="https://www.youtube.com/watch?v=QDbjsABOnAU">https://www.youtube.com/watch?v=QDbjsABOnAU</a> </h3>
+    </div>
+</div>
+
+</section>
+
+<!-- about section ends -->
+</body>
+</html>
